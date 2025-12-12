8100162796830697a87e7399ca0ccb5149b8945f
Huh... <rel270606@gmail.com>
2025-12-12 20:33:51 +1000

    login page

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {observer} from "mobx-react-lite";
 import LoginBackground from "../../components/LoginPage/LoginBackground/LoginBackground.tsx";
 import LoginForm from "../../components/LoginPage/LoginForm/LoginForm.tsx";
+//fix
 
 const LoginPage: React.FC = () => {
     return (
